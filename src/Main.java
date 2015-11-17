8563c5019e14bbed2cfdfa088366391928c9373a
dgdeleonardis <diegowazzaap@hotmail.it>
2015-11-17 09:48:20 +0100

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String argv[]){
+        float distanza;
+        
+        Punto P0 = new Punto(2,4);
+        Punto P1 = new Punto(-2,1);
+        
+        distanza = P1.distanzaOrigine();
+        System.out.println("La distanza di P1 dall'origine e': " + distanza);
+        
+        distanza = P1.distanzaAltroPunto(P0);
+        System.out.println("La distanza di P1 da P0 e': " + distanza);
+        
+        
+    }
+}
