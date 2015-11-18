0e8617e0f08877b58c7f7e0990a34d9455a999c8
Matteo Calosci <matteo1789@live.it>
2015-11-18 10:54:30 +0100

    PRIMO

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,15 @@ public class Main {
         Punto P0 = new Punto(2,4);
         Punto P1 = new Punto(-2,1);
         
+        Cerchio c1 = new Cerchio();
+        
         distanza = P1.distanzaOrigine();
         System.out.println("La distanza di P1 dall'origine e': " + distanza);
         
         distanza = P1.distanzaAltroPunto(P0);
         System.out.println("La distanza di P1 da P0 e': " + distanza);
+        
+        
         
         
     }
